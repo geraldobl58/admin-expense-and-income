00d52e690fd977f71525ca85bf0f149152adc6ba
Geraldo Luiz <geraldobl58@gmail.com>
2021-03-04 16:30:43 -0300

    Feature: add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components/macro';
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html, body, #root {
+        height: 100%;
+        font-family: 'Roboto', sans-serif;
+    }
+
+    button: {
+        cursor: pointer;
+    }
+`;
