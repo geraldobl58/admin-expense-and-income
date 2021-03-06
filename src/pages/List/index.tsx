0dd267e56c75f41d9363d3044f0b5602df5f5c9b
Geraldo Luiz <geraldobl58@gmail.com>
2021-03-06 17:54:03 -0300

    Feature: add filter styles

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -4,21 +4,42 @@ import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import FinancialCard from '../../components/FinancialCard';
 
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 
 const List: React.FC = () => {
 
-    const options = [
-        { value: 'Janeiro', label: 'Janeiro' },
-        { value: 'Fevereiro', label: 'Fevereiro' },
-        { value: 'Março', label: 'Março' },
+    const months = [
+        { value: 3, label: 'Março' },
+        { value: 4, label: 'Abril' },
+        { value: 5, label: 'Maio' },
+    ];
+
+    const years = [
+        { value: 2021, label: 2021 },
+        { value: 2022, label: 2022 },
+        { value: 2023, label: 2023 },
     ];
 
     return (
         <Container>
-            <ContentHeader title="Entrada" lineColor="green">
-                <SelectInput options={options} />
+            <ContentHeader title="Saídas" lineColor="#e44c4e">
+                <SelectInput options={months} />
+                <SelectInput options={years} />
             </ContentHeader>
+            <Filters>
+                <button 
+                    type="button" 
+                    className="tag-filter tag-filter-recurrent"
+                >
+                    Recorrentes
+                </button>
+                <button 
+                    type="button" 
+                    className="tag-filter tag-filter-eventual"
+                >
+                    Eventuais
+                </button>
+            </Filters>
             <Content>
                 <FinancialCard 
                     tagColor='#e44c4e'
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -3,3 +3,46 @@ import styled from 'styled-components/macro';
 export const Container = styled.div``;
 
 export const Content = styled.div``;
+
+export const Filters = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+
+    margin-bottom: 20px;
+
+    .tag-filter {
+        font-size: 18px;
+        font-weight: 100;
+        font-family: 'Montserrat', sans-serif; 
+        background: none;
+        border: none;
+        cursor: pointer;
+        text-transform: uppercase;
+        color: ${props => props.theme.colors.white};
+
+        margin: 0 10px;
+
+        transition: opacity .3s;
+
+        &:hover {
+            opacity: .7;
+        }
+    }
+
+    .tag-filter-recurrent::after {
+        content: '';
+        display: block;
+        width: 55px;
+        margin: 0 auto;
+        border-bottom: 5px solid ${props => props.theme.colors.warning};
+    }
+
+    .tag-filter-eventual::after {
+        content: '';
+        display: block;
+        width: 55px;
+        margin: 0 auto;
+        border-bottom: 5px solid ${props => props.theme.colors.success};
+    }
+`;
