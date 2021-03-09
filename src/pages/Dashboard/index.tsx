456254559c4ed684a98f66376b2386132c6ee515
Geraldo Luiz <geraldobl58@gmail.com>
2021-03-09 11:10:51 -0300

    Feature: add filter page dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,22 +1,80 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 
+import expenses from '../../repositories/expenses';
+import gains from '../../repositories/gains';
+
+import listOfMonths from '../../utils/months';
+
 import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
+    const [monthSelected, setMonthSelected] = useState<number>(new Date().getMonth() + 1);
+    const [yearSelected, setYearSelected] = useState<number>(new Date().getFullYear());
 
-    const options = [
-        { value: 'Janeiro', label: 'Janeiro' },
-        { value: 'Fevereiro', label: 'Fevereiro' },
-        { value: 'Março', label: 'Março' },
-    ];
+    const years = useMemo(() => {
+        let uniqueYears: number[] = [];
+
+        [...expenses, ...gains].forEach(item => {
+            const date = new Date(item.date);
+            const year = date.getFullYear();
+
+            if (!uniqueYears.includes(year)) {
+                uniqueYears.push(year);
+            }
+        });
+
+        return uniqueYears.map((year) => {
+            return {
+                value: year,
+                label: year,
+            }
+        });
+
+    }, []);
+
+    const months = useMemo(() => {
+        return listOfMonths.map((month, index) => {
+            return {
+                value: index + 1,
+                label: month,
+            }
+        });
+    }, []);
+
+    const handleMonthSelected = (month: string) => {
+        try {
+            const parseMonth = Number(month);
+            setMonthSelected(parseMonth);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    const handleYearSelected = (year: string) => {
+        try {
+            const parseYear = Number(year);
+            setYearSelected(parseYear);
+        } catch(error) {
+            console.log(error);
+        }
+    }
 
     return (
         <Container>
             <ContentHeader title="Dashboard" lineColor="orange">
-                <SelectInput options={options} onChange={() => {}} />
+                <SelectInput 
+                    options={months} 
+                    onChange={(e) => handleMonthSelected(e.target.value)}
+                    defaultValue={monthSelected} 
+                />
+                <SelectInput 
+                    options={years} 
+                    onChange={(e) => handleYearSelected(e.target.value)}
+                    defaultValue={yearSelected} 
+                />
             </ContentHeader>
         </Container>
     )
